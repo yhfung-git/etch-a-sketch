e8cba390f8b3754b116fc6e756ac922930e9d4ae
yonghowfung <yhfung940217@gmail.com>
2023-02-03 20:33:08 +0100

    sudocode for game

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
+// Game
+// Select Size (from 2x2 until 99x99)
+// Select Color (one / colorful)
+// Eraser (white)
+// Reset All (clear)
+
+
 // Current Year
 date = new Date();
 year = date.getFullYear();
